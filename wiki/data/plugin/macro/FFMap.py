296f73dff86cc5a57096560039cd86a440a86a38
Felix Möller <git@felix-moeller.net>
2015-11-25 05:28:23 +0100

    Fixed #4

--- wiki/data/plugin/macro/FFMap.py
+++ wiki/data/plugin/macro/FFMap.py
@@ -1,2 +1,2 @@
 def macro_FFMap(macro):
-	return "<iframe src=\"https://map.freifunk-kassel.de/meshviewer\" width=\"100%\" height=\"600px\" frameBorder=\"0\"></iframe>"
+	return "<iframe src=\"https://map.freifunk-kassel.de/\" width=\"100%\" height=\"600px\" frameBorder=\"0\"></iframe>"
