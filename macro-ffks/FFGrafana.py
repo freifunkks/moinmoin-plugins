41ce439dd5a7e85419fedcf27595798d13013da0
Felix Möller <git@felix-moeller.net>
2016-01-26 07:43:42 +0100

    Add grafana iframe macro

---
+++ macro-ffks/FFGrafana.py
@@ -0,0 +1,5 @@
+def macro_FFGrafana(macro, days=7, height=300):
+	height = int(height)
+	if height <= 0:
+		return 'Error with stats height'
+        return '<iframe src="https://stats.freifunk-kassel.de/dashboard-solo/db/overview?panelId=1&fullscreen&theme=light&days=%s" width="100%" height="%s" frameborder="0"></iframe>' % (days, height)
