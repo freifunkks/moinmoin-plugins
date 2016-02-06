07497bae909d843c0760ba060986dfd1d6e575af
Felix Möller <git@felix-moeller.net>
2016-02-06 04:55:18 +0100

    Fix grafana macro

--- macro-ffks/FFGrafana.py
+++ macro-ffks/FFGrafana.py
@@ -2,4 +2,4 @@ def macro_FFGrafana(macro, days=7, height=300):
 	height = int(height)
 	if height <= 0:
 		return 'Error with stats height'
-        return '<iframe src="https://stats.freifunk-kassel.de/dashboard-solo/db/overview?panelId=1&fullscreen&theme=light&days=%s" width="100%" height="%s" frameborder="0"></iframe>' % (days, height)
+        return '<iframe src="https://stats.freifunk-kassel.de/dashboard-solo/db/overview?panelId=1&fullscreen&theme=light&from=now-%sd&to=now" width="100%" height="%s" frameborder="0"></iframe>' % (days, height)
