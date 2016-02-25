ef484f7d282f5ae226206bb2ff095f0c74fe9505
Felix Möller <git@felix-moeller.net>
2016-02-25 16:16:00 +0100

    Fix URL

--- macro-ffks/FFGrafana.py
+++ macro-ffks/FFGrafana.py
@@ -2,4 +2,4 @@ def macro_FFGrafana(macro, days=7, height=300):
 	height = int(height)
 	if height <= 0:
 		return 'Error with stats height'
-	return '<iframe src="https://stats.freifunk-kassel.de/dashboard-solo/file/overview.json?panelId=1&fullscreen&theme=light&from=now-{0}d&to=now" width="100%" height="{1}" frameborder="0"></iframe>'.format(days, height)
+	return '<iframe src="https://stats.freifunk-kassel.de/?panelId=1&fullscreen&theme=light&from=now-{0}d&to=now" width="100%" height="{1}" frameborder="0"></iframe>'.format(days, height)
