5bf2c57280c5602feb51907f960b3ad717f5f4c2
Jonas Platte <mail@jonasplatte.de>
2016-02-27 18:35:58 +0100

    FFImageChooser: Read channel and release from yml file, allow different file extensions

--- macro-ffks/FFImageChooser.py
+++ macro-ffks/FFImageChooser.py
@@ -8,18 +8,18 @@ import yaml
 from MoinMoin import config
 from MoinMoin.action import AttachFile
 
+
 def utf_8_encoder(unicode_csv_data):
     for line in unicode_csv_data:
         yield line.encode('utf-8')
 
+
 def get_yaml(file, attach_dir):
     return yaml.load(codecs.open(os.path.join(attach_dir, file), 'rb', config.charset))
 
-def macro_FFImageChooser(macro):
-    vendors_file = 'vendors.yml'
 
-    # TODO: Add this to the yml file?
-    release = 'v2016.1-1'
+def macro_FFImageChooser(macro):
+    data_file = 'data.yml'
 
     request = macro.request
     formatter = macro.formatter
@@ -30,21 +30,24 @@ def macro_FFImageChooser(macro):
 
     # vendors
     try:
-        vendors_idx = files.index(vendors_file)
+        data_idx = files.index(data_file)
     except ValueError:
-        return "Konnte Dateianhang " + vendors_file + " nicht finden"
-    vendors = get_yaml(files[vendors_idx], attach_dir)
+        return "Konnte Dateianhang " + data_file + " nicht finden"
+
+    data = get_yaml(files[data_idx], attach_dir)
 
     # output
     ret = '<div class="download-form">'
 
-    for vendor in vendors:
+    for vendor in data.vendors:
         ret += '''
             <input type="radio" name="vendor" class="vendor" id="vendor-%(id)s" />
             <label for="vendor-%(id)s">%(name)s</label>
             <div class="device-list">''' % vendor
 
-        for device in vendor['devices']:
+        for device in vendor.devices:
+            extension = device.extension if 'extension' in device else 'bin'
+
             ret += '<div class="device-wrap">'
 
             if 'versions' in device:
@@ -52,24 +55,31 @@ def macro_FFImageChooser(macro):
                     <input type="radio" name="device" class="device" id="device-%(vendor_id)s-%(device_id)s" />
                     <label for="device-%(vendor_id)s-%(device_id)s">%(device_name)s</label>
                     <div class="version-list">
-                    ''' % {'device_id': device['id'], 'device_name': device['name'], 'vendor_id': vendor['id']}
-                for version in device['versions']:
+                    ''' % {'device_id': device.id, 'device_name': device.name, 'vendor_id': vendor.id}
+                for version in device.versions:
                     try:
-                        version_label = version['name']
+                        version_label = version.name
                     except:
-                        version_label = version['id']
+                        version_label = version.id
 
                     ret += '''
-                        <a class="version" href="https://dl.freifunk-kassel.de/images/stable/factory/gluon-ffks-%(release)s-%(vendor)s-%(device)s-%(version)s.bin">
+                        <a class="version" href="https://dl.freifunk-kassel.de/images/%(chan)s/factory/gluon-ffks-%(rel)s-%(vendor)s-%(device)s-%(version)s.%(ext)s">
                             %(text)s
-                        </a>''' % {'release': release, 'vendor': vendor['id'], 'device': device['id'], 'version': version['id'], 'text': version_label}
+                        </a>''' % {
+                        'chan': data.channel, 'rel': data.release, 'vendor': vendor.id,
+                        'device': device.id, 'version': version.id, 'text': version_label,
+                        'ext': extension
+                    }
 
                 ret += '</div>'
             else:
                 ret += '''
-                    <a class="device" href="https://dl.freifunk-kassel.de/images/stable/factory/gluon-ffks-%(release)s-%(vendor)s-%(device)s.bin">
+                    <a class="device" href="https://dl.freifunk-kassel.de/images/%(chan)s/factory/gluon-ffks-%(rel)s-%(vendor)s-%(device)s.%(ext)s">
                         %(text)s
-                    </a>''' % {'release': release, 'vendor': vendor['id'], 'device': device['id'], 'text': device['name']}
+                    </a>''' % {
+                    'chan': data.channel, 'rel': data.release, 'vendor': vendor.id,
+                    'device': device.id, 'text': device.name, 'ext': extension
+                }
 
             ret += '</div>'
 
