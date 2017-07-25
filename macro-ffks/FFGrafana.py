eca9c9f18928f356fb831282516a09d6c80b0912
Felix Möller <git@felix-moeller.net>
2017-07-25 13:52:43 +0200

    Revert "Update FFGrafana macro to create images instead of iframes"
    This reverts commit 7bc224ae1957402fda7275e4c11c8e6ee2ed1f4e.

--- macro-ffks/FFGrafana.py
+++ macro-ffks/FFGrafana.py
@@ -1,9 +1,5 @@
-def macro_FFGrafana(macro, days=7, width=600, height=400):
-	width  = int(width)
+def macro_FFGrafana(macro, days=7, height=300):
 	height = int(height)
 	if height <= 0:
 		return 'Error with stats height'
-	if width <= 0:
-		return 'Error with stats width'
-
-	return '<img src="https://stats.freifunk-kassel.de/render/dashboard-solo/file/overview.json?panelId=1&fullscreen&theme=light&from=now-{0}d&to=now&width={1}&height={2}" alt="Statistik-Graph" />'.format(days, width, height)
+	return '<iframe src="https://stats.freifunk-kassel.de/dashboard-solo/file/overview.json?panelId=1&fullscreen&theme=light&from=now-{0}d&to=now" width="100%" height="{1}" frameborder="0"></iframe>'.format(days, height)
